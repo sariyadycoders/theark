245c5c4208677b298e4ee6669aa1b296fd2fcb23
sariya <sariya@dycoders.com>
2024-07-16 16:41:54 +0500

    month options completion

--- lib/the_ark_web/live/home_live.ex
+++ lib/the_ark_web/live/home_live.ex
@@ -33,10 +33,18 @@ defmodule TheArkWeb.Home do
 
   @month_options [
     {"None", 0},
-    {"Jan", 1},
-    {"Feb", 2},
-    {"Mar", 3},
-    {"April", 4}
+    {"January", 1},
+    {"February", 2},
+    {"March", 3},
+    {"April", 4},
+    {"May", 5},
+    {"June", 6},
+    {"July", 7},
+    {"August", 8},
+    {"September", 9},
+    {"October", 10},
+    {"November", 11},
+    {"December", 12}
   ]
 
   @days_of_week ~w(Mon Tue Wed Thu Fri Sat Sun)
